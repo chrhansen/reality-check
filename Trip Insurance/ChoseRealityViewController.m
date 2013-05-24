8e80cca8ba6fe4b41766b06fcef6da16e9135e3b
Christian Hansen <christiandanmark@gmail.com>
2013-05-24 15:09:51 -0700

    minor refactoring

--- Trip Insurance/ChoseRealityViewController.m
+++ Trip Insurance/ChoseRealityViewController.m
@@ -45,15 +45,6 @@ typedef enum {
 
 @implementation ChoseRealityViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
